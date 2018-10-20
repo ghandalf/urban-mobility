c8dd726a6ff07a048069a83f6657429aa242d8f8
ghandalf <1793338+ghandalf@users.noreply.github.com>
2018-10-19 20:48:34 -0400

    Prepare the Agency transport information.

--- rest-api/src/main/java/ca/ghandalf/urban/mobility/restapi/AgencyController.java
+++ rest-api/src/main/java/ca/ghandalf/urban/mobility/restapi/AgencyController.java
@@ -1,6 +1,9 @@
 package ca.ghandalf.urban.mobility.restapi;
 
 import java.util.List;
+import java.util.UUID;
+
+import javax.validation.Valid;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,7 +11,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -32,7 +40,7 @@ public class AgencyController {
 	@Autowired
 	private AgencyService service;
 	
-	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(path = "/agencies", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(value = HttpStatus.OK)
 	public @ResponseBody List<Agency> findAll() {
 		if (service.findAll().isPresent()) {
@@ -44,4 +52,51 @@ public class AgencyController {
 		}
 		
 	}
+
+	@PostMapping(path = "/crateagency")
+	public @ResponseBody Agency create(@RequestBody @Valid Agency entity) {
+		if ( service.create(entity).isPresent() ) {
+			return service.create(entity).get();
+		} else {
+			// Error management
+			logger.debug("Unable to create the agency");
+			return null;
+		}
+	}
+	
+	@GetMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody Agency read(@PathVariable(name = "id", required = true) UUID id ) {
+		if ( service.read(id).isPresent() ) {
+			return service.read(id).get();
+		} else {
+			logger.debug("The agency identifier don't exist.");
+			return null;
+		}
+	}
+
+	@PutMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody Agency update(@RequestBody @Valid Agency entity) {
+		if ( service.update(entity).isPresent() ) {
+			return service.update(entity).get();
+		} else {
+			logger.debug("Unable to update the entity.");
+			return null;
+		}
+	}
+	
+	@DeleteMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public void delete(@RequestBody @Valid Agency entity) {
+		service.delete(entity);
+	}
+	
+	@GetMapping(path = "/{agencyId}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody Agency findByAgencyId(@PathVariable(name = "agencyId", required = true) String agencyId) {
+		if ( service.findByAgencyId(agencyId).isPresent() ) {
+			return service.findByAgencyId(agencyId).get();
+		} else {
+			logger.debug("This agencyId: [{}] doesn't exist.", agencyId);
+			return null;
+		}
+	}
+	
 }
---
+++ rest-api/src/main/java/ca/ghandalf/urban/mobility/restapi/exception/AgencyNotFoundException.java
@@ -0,0 +1,7 @@
+package ca.ghandalf.urban.mobility.restapi.exception;
+
+public class AgencyNotFoundException extends Exception {
+
+	private static final long serialVersionUID = -4146094295520530026L;
+	
+}
