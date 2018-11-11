ddb5010eb2b544ff51650b47e8f963780c3d6d8e
ghandalf <1793338+ghandalf@users.noreply.github.com>
2018-11-10 21:38:55 -0500

    Use docker-compose, create two file one for local work second for swarm in prod

--- scripts/docker/docker.sh
+++ scripts/docker/docker.sh
@@ -7,108 +7,166 @@
 ## Author: Ghandalf
 ###
 
+application=$0
 command=$1
-
-function usage() {
-    echo -e "\n\tUsage:";
-    echo -e "\t\t$0 <clean|db|start|show|stop>";
-    echo -e "\n";
-}
-
+args=$2
 
 function start() {
-    echo -e "\n\tStarting containers";
+    echo -e "\n\tStarting containers...";
 
 	for i in "${images[@]}"; do
-    case $i in 
-        sath89/oracle-12c)    
-            # see: https://hub.docker.com/r/sath89/oracle-12c/
-            # Oracle created for Fresche Solutions
-            # -d will start the container in detached mode
-            # -p dedicated ports 8080 will be used by http://localhost:8080/apex tools and 1521 by databases sqlplus command line or tools
-            # Login on http://localhost:8080/apex asked for user: admin and new password::Ghandalf+12
-            # docker run -d -p 8080:8080 -p 1521:1521 --name docker-oracle -v /data/app/db/OracleDataDocker/oracle:/u01/app/oracle sath89/oracle-12c
-            docker run -d -p 8080:8080 -p 1521:1521 --name docker-oracle -v ${oracle12_data}:/u01/app/oracle $i
-            echo -e "\n\t Oracle 12 started\n";
-            ;;
-        dockerhelp/docker-oracle-ee-18c)    
-            # see: https://hub.docker.com/r/sath89/oracle-12c/
-            # Oracle created for Fresche Solutions
-            # -d will start the container in detached mode
-            # -p dedicated ports 8080 will be used by http://localhost:8080/apex tools and 1521 by databases sqlplus command line or tools
-            # Login on http://localhost:8080/apex asked for user: admin and new password::Ghandalf+12
-            # docker run -d -p 8080:8080 -p 1521:1521 --name docker-oracle -v /data/app/db/OracleDataDocker/oracle:/u01/app/oracle sath89/oracle-12c
-            docker run -d -p 8090:8090 -p 1523:1523 --name docker-oracle18 -v ${oracle18_data}:/u01/app/oracle18 $i
-            echo -e "\n\t Oracle 18 started\n";
-            ;;
-        postgres)
-            # see: https://hub.docker.com/r/library/postgres/
-            #docker run -d -p 5432:5432 --name docker-postgres -v /data/app/db/PostgreSQLDataDocker/postgresql -e POSTGRES_PASSWORD=postgres postgres
-            #docker run -d -p 5432:5432 --name docker-postgres -v postgresql-data:/data/app/db/PostgreSQLDataDocker/postgresql -e POSTGRES_DB=UrbanMobility -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres postgres
-            docker run -d -p 5432:5432 --name docker-postgres -v ${postgres_data}:/u01/app/postgres -e POSTGRES_DB=UrbanMobility -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres $i
-            #docker start 0a3e218c1b36
-            echo -e "\n\t PostgreSQL started\n";
-            # Connect directly on command line to postgresql
-            # docker run -it --rm --link docker-postgres:postgres postgres psql -h postgres -U postgres
-            # psql -h 127.0.0.1 -p 5432 -U postgres
-            # psql -h 127.0.0.1 -p 5432 -U urbanadmin
- 			# psql -h 127.0.0.1 -p 5432 -U postgres
-            # psql -h 127.0.0.1 -U urbanadmin Urb@n@dmnetstat -na | grep 8080
-            # psql -h 127.0.0.1 -U urbanadmin -p Urb@n@dmnetstat -na | grep 8080
-            # psql -h 127.0.0.1 -U urbanadmin -p 5432
-            # psql -h localhost -U postgres 
-            
-            # createUser;
-            ;;
-        redis)
-            # see: https://hub.docker.com/_/redis/
-            #docker run -d -p 6379:6379 --name docker-redis -v /data/app/db/RedisDataDocker/redis:/u01/app/redis redis
-            docker run -d -p 6379:6379 --name docker-redis -v ${redis_data}:/u01/app/redis $i
-            echo -e "\n\t Redis started\n";
-            ;;
-        *)
-            echo -e "\n\t Please provide a container: <oracle|postgres|redis> \n";
-            ;;
-    esac  
+	    case $i in 
+	        dockerhelp/docker-oracle-ee-18c)    
+	            docker run --name docker-oracle18c -d -p 8090:8090 -p 1523:1523 -v ${oracle18_data}:/opt/app/oracle18 $i
+    	        echo -e "\n\t Oracle 18c started with data: ${oracle18_data}\n";
+            	;;
+        	sath89/oracle-12c)    
+            	# docker run -d -p 8080:8080 -p 1521:1521 --name docker-oracle -v /data/app/db/OracleDataDocker/oracle:/u01/app/oracle sath89/oracle-12c
+            	docker --name docker-oracle12c run -d -p 8080:8080 -p 1521:1521 -v ${oracle12_data}:/opt/app/oracle $i
+            	echo -e "\n\t Oracle 12c started with data: ${oracle12_data}\n";
+            	;;
+        	postgres)
+            	#docker run -d -p 5432:5432 --name docker-postgres -v ${postgres_data} -e POSTGRES_PASSWORD=postgres postgres
+            	#docker run -d -p 5432:5432 --name docker-postgres -v postgresql-data:/data/app/db/PostgreSQLDataDocker/postgresql -e POSTGRES_DB=UrbanMobility -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres postgres
+            	docker run --name docker-postgres -d -p 5432:5432 -v ${postgres_data}:/opt/app/postgres -e POSTGRES_DB=UrbanMobility -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres $i
+            	#docker start 0a3e218c1b36
+            	echo -e "\n\t PostgreSQL started with data:${postgres_data}\n";
+            	;;
+        	redis)
+            	docker run --name docker-redis -d -p 6379:6379 -v ${redis_data}:/opt/app/redis $i
+            	echo -e "\n\t Redis started\n";
+            	;;
+	    esac  
     done
 }
 
+###
+# Create analytic network if it doesn't exist
+# This network will be use for the communication in between analytics containers.
+##
+function createDockerNetwork() {
+    
+    local result=`docker network ls | grep ${analytic_network} | awk {'printf "%s\n", $2'}`
+    # echo -e "RESULT: $result";
+    if [ $result ]; then
+        echo -e "The docker network [${analytic_network}] already exists.";
+    else 
+        echo -e "Creating docker network [${analytic_network}]";
+        docker network create ${analytic_network} --driver=bridge
+    fi
+}
+
+function removeDockerNetwork() {
+    docker network rm ${analytic_network}
+}
+
+###
+# Start the analytice containers link together thru a bridge network
+##
+function startAnalytic() {
+
+    createDockerNetwork;
+    
+    local result;
+    for i in "${kibana_images[@]}"; do
+        
+        containerName=`echo $i | awk -F'/' {'printf $3'} | awk -F':' {'printf $1'}`
+        result=`docker container ls -q -f name=$containerName`
+        
+        if [ $result ]; then 
+            echo -e "\n\tContainer $containerName is running will be stop, remove and restart it";
+            docker stop $containerName;
+            docker rm $containerName;
+        fi
+
+        # echo -e "\n Starting container [$containerName]";
+        case $containerName in
+            elasticsearch)
+                # see: httpans://www.elastic.co/guide/en/elasticsearch/reference/current/docker.html
+                docker run --name $containerName -p 9200:9200 -p 9300:9300 -d --network ${analytic_network} $i
+                ;;
+            kibana)
+                docker run --name $containerName -p 5601:5601 -d --network ${analytic_network} $i;
+                ;;
+        esac
+    done
+}
+
+function startSwarm() {
+	docker swarm leave --force
+	docker swarm init;
+	# to clean up: docker swarm leave --force
+	
+	#To add a worker to this swarm, run the following command:
+    	# docker swarm join --token SWMTKN-1-4otvtlk254dnaog6gvoka1ketgcacvxvw2dohueu16fkwgjr0g-bwk926il697dtyxrx8q23o2hx 10.10.157.38:2377
+	# To add a manager to this swarm, run 'docker swarm join-token manager' and follow the instructions.
+	docker stack deploy -c container/docker-compose.prod.yml analytic-stack
+}
+
+function updateDockerCompose() {
+	sudo curl -L "https://github.com/docker/compose/releases/download/${compose_version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
+}
+
+function ports() {
+    netstat -na | grep -e 5601 -e 5602 -e 5603 -e 5604 -e 5605 -e 5606 -e 5607 -e 5608 -e 5609 -e 9200
+}
 
 function show() {
     echo -e "\n\tDocker container installed";
     docker images
     echo -e "\n\tDocker running";
     docker ps 
+    echo -e "\n\tDocker network";
+    docker network ls
+    echo -e "\n\tDocker service";
+    docker service ls
+	echo -e "\n\tValidate the container/docker-compose.yml file";
+	docker-compose -f container/docker-compose.yml config;
+	echo -e "\n\tValidate the container/docker-compose.prod.yml file";
+	docker-compose -f container/docker-compose.prod.yml config;
     echo -e "\n";
-}
-
-function startImages() {
-	length="${#imagesId[@]}";
-	for (( i=0; i<$length; i++)); do
-		docker start "${imagesId[$i]}"
-		#echo -e "${imagesId[$i]}"
-	done
 }
 
 function stop() {
     echo -e "\n\tStopping all containers\n";
-    docker stop $(docker ps -aq)
+    docker container stop $(docker ps -aq)
     echo -e "\n\tAll containers stopped\n";   
 }
 
 function cleanUp() {
-    echo -e "\t\n Remove unused volumes\n"; 
-    # docker volume rm $(docker volume ls -f dangling=true);
-    #docker volume ls
-    #docker volume prune
-    docker volume ls
-    
+
     echo -e "\n\t Remove background running containers\n"
     docker stop $(docker ps -aq)
     docker rm $(docker ps -aq)
+	docker network rm ${analytic_network}
+	docker swarm leave --force
+	
     echo -e "\n\t Containers removed \n"
-    docker ps -a
+    docker ps -aq
+    
+    echo -e "\n\t Network removed \n"
+    docker network ls
+    
+    echo -e "\n";
+}
 
+function loadResources() {
+	echo -e "\n\t\t $0 load resources...";
+	if [ -f ./container/docker.properties ]; then
+		source ./container/docker.properties;
+	else
+		echo -e "\n\t\tYou need to provide the file docker.properties under container directory...";
+		echo -e "\n";
+	fi
+}
+
+function pull() {
+    for i in "${images[@]}"; do
+        docker pull $i;
+    done
+    for i in "${kibana_images[@]}"; do
+        docker pull $i;
+    done
 }
 
 function curl() {
@@ -163,59 +221,20 @@ function db() {
 		user)
 			createUser;
 			;;
+		startSwarm)
+			startSwarm;
+			;;
 		*)
             echo -e "\n\t Please provide a command: <create|insert|drop|user> \n";
             ;;
 	esac
 }
 
-function loadResources() {
-	echo -e "\n\t\t $0 load resources...";
-	if [ -f `pwd`/scripts/docker/container/docker.properties ]; then
-		source `pwd`/scripts/docker/container/docker.properties;
-	else
-		echo -e "\n\t\tYou need to provide the file docker.properties under container directory...";
-	fi
+function usage() {
+    echo -e "\n\tUsage:";
+    echo -e "\t\t$0 <pull|clean|db|start|show|stop>";
+    echo -e "\n";
 }
-
-
-function pull() {
-	for i in "${images[@]}"; do
-		echo "$i"
-		docker pull $i;
-	done
-}
-
-function pullAnalytic() {
-	for i in "${imagesAnalytic[@]]}"; do
-		docker pull $i;
-	done
-}
-
-###
-# Before we start analytics container we need to create a docker network
-# https://stackoverflow.com/questions/40373400/docker-compose-yml-for-elasticsearch-and-kibana
-##
-function startAnalytic() {
-	# Create the network for communication in between containers
-	#docker network create analytic-net --driver=bridge
-	
-	for i in "${imagesAnalytic[@]]}"; do
-		currentName=`echo $i | awk -F'/' {'printf $3'} | awk -F':' {'printf $1'}`
-
-		case $currentName in
-			elasticsearch)
-				docker run --name $currentName -p 9200:9200 -p 9300:9300 -d --network analytic-net $i
-				#docker run --name elasticsearch -p 9200:9200 -p 9300:9300 -d --network analytic-net docker.elastic.co/elasticsearch/elasticsearch:6.4.3
-				;;
-			kibana)
-				docker run --name $currentName -p 5601:5601 -d --network analytic-net $i 
-				;;
-		esac
-		echo -e "\n\t $currentName started\n";
-	done
-}
-
 
 function finish() { 
 	echo -e "\n\t\tUse to clean resources before we live\n";
@@ -223,15 +242,10 @@ function finish() {
 trap finish EXIT;
 
 loadResources;
+
 case ${command} in
 	pull)
 		pull;
-		;;
-	pullAnalytic)
-		pullAnalytic;
-		;;
-	startAnalytic)
-		startAnalytic;
 		;;
     clean)
         cleanUp;
@@ -242,9 +256,12 @@ case ${command} in
     start)
         start $2;
         ;;
-    startImages)
-    	startImages;
-    	;;
+	startAnalytic)
+		startAnalytic;
+		;;
+	startSwarm)
+		startSwarm;
+		;;
     show)
         show;
         ;;
@@ -254,6 +271,9 @@ case ${command} in
     curl)
         curl $2;
         ;;
+	updateDockerCompose)
+		updateDockerCompose;
+		;;
     *) usage;
 esac
 
