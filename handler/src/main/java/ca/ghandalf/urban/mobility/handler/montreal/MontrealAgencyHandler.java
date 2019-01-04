cb0b32f34f83daa34d177c3c1ba533256c3b4ecd
ghandalf <Ouellet.Francis@gmail.com>
2019-01-03 20:26:13 -0500

    Add component to the class

---
+++ handler/src/main/java/ca/ghandalf/urban/mobility/handler/montreal/MontrealAgencyHandler.java
@@ -0,0 +1,10 @@
+package ca.ghandalf.urban.mobility.handler.montreal;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+@Component
+@ComponentScan(basePackages = { "ca.ghandalf" })
+public class MontrealAgencyHandler {
+
+}
