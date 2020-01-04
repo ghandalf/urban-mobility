694747f440e22d278e4a59a46dbb8f5d71fa388c
ghandalf <1793338+ghandalf@users.noreply.github.com>
2020-01-04 17:11:54 -0500

    Normalize infra and use postgres container

--- um.sh
+++ um.sh
@@ -1,15 +1,54 @@
 #!/bin/bash
 
-current_dir=`pwd`
-
-# we execute this script from scripts directory need to move under urban-mobility
-parent_dir=../
+current_dir=$(pwd);
+links_dir="/data/links";
+jdk_dir="/data/app/programs/java";
+jdk_needed="/data/app/programs/java/jdk-13.0.1";
+jdk_path_file="${links_dir}/jdk.original.link";
 
 command=$1
 module=$2
 
+function setUp() {
+    if [ -h ${links_dir}/jdk ]; then
+	    echo -e "${n1t}${Green}Set up jdk to [${Cyan}${jdk_needed}${Green}]${Color_Off}";
+        cd ${links_dir};
+        echo "jdk_path=$(realpath jdk)" > ${jdk_path_file}; 
+        rm -f jdk;
+        ln -s ${jdk_needed} jdk;
+        cd ${current_dir};
+        echo -e "${tab}${Green}Set up jdk to [${Cyan}${jdk_needed}${Green}] ${Yellow}done${Color_Off}${nl}";
+    fi
+    
+    echo -e "${n1t}${Green}Starting postgres container${Color_Off}";
+    /usr/bin/docker info >/dev/null 2>&1;
+    if [[ ! $? -eq 0  ]]; then 
+    	sudo service docker start; 
+    fi
+    docker-compose -f docker-compose.yml up -d;
+    echo -e "${tab}${Green}Starting postgres container ${Yellow}done${Color_Off}${nl}";
+}
+
+function tearDown() {
+	# JDK config
+    if [ -f ${jdk_path_file} ]; then
+        source ${jdk_path_file};
+		echo -e "${n1t}${Green}Rolling back jdk to [${Cyan}${jdk_path}${Green}]${Color_Off}";
+        cd ${links_dir};
+        rm -f jdk;
+        ln -s ${jdk_path} jdk;
+        rm -f ${jdk_path_file};
+        cd ${current_dir};
+        echo -e "${tab}${Green}Rolling back jdk to [${Cyan}${jdk_path}${Green}] ${Yellow}done${Color_Off}${nl}";
+    fi
+    
+    echo -e "${n1t}${Green}Shutdown postgres container${Color_Off}";
+    # Postgres config
+    docker-compose -f docker-compose.yml down;
+    echo -e "${tab}${Green}Shutdown postgres container ${Yellow}done${Color_Off}${nl}";
+}
+
 function generate() {
-    cd ${parent_dir}
 
     case ${module} in
         domain)
@@ -56,23 +95,39 @@ function generate() {
         ;;
     esac
 
-    cd ${current_dir}
 }
 
 function show() {
-    cd ${parent_dir}
     jdeps generated-modules/
-    cd ${current_dir}
 }
 
+function run() { 
+	local ws_dir=ws-api;
+
+	mvn enforcer:display-info;
+	mvn clean install -DskipTests;
+	cd ${ws_dir};
+	mvn spring-boot:run;
+	
+	cd ${current_dir};
+}
 
 function usage() {
     echo -e "\n";
-    echo -e "\t\t$0 command=<gen|show> project=<domain|repository|handler|service|rest>";
+    echo -e "\t\t$0 command=<build|gen|show> project=<domain|repository|handler|service|rest>";
     echo -e "\n";
 }
 
-case $command in 
+source scripts/resources/colors.properties;
+source scripts/resources/formatter.properties;
+setUp;
+case ${command} in 
+    build)
+        mvn clean install -DskipTests; 
+        ;;
+    run)
+        run; 
+        ;;
     gen)
         generate;
     ;;
@@ -82,5 +137,5 @@ case $command in
     *)
         usage;
     ;;
-esac
- 
+esac 
+tearDown;
